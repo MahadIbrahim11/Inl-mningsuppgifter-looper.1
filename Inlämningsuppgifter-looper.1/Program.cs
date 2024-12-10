e4197de7342ffda46adbb1f81a9913474239fe4c
maib07001 <maib07001@E-PW05C37V>
2024-12-10 18:58:17 +0100

    Add project files.

---
+++ Inlämningsuppgifter-looper.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace inl_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 10 till 30
+            for (int i = 10; i <= 30; i++)
+            {
+                Console.WriteLine(i);
+            }
+            
+            // 200 till 180
+            for (int i = 200; i >= 180; i--)
+            {
+                Console.WriteLine(i);
+            }
+
+            // 1000 till 1400
+            for (int i = 1000; i <= 1400; i += 50)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
